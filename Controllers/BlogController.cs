3d0048e9d6fdab762ea03eda6d2e1b39a3871ecd
chewi-ng <chewing.8.18@gmail.com>
2020-07-06 21:58:38 +0200

    ..

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,43 @@
+﻿using SimpleBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Articles()
+        {
+            var posts = new Article() {
+                Title= "title 1",
+                Body= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+                Description= "Lorem ipsum",
+                Author= "First Author",
+                //Published= DateTime.Parse("06/25/2020 20:37:02")
+            };
+            List<Article> list = new List<Article>
+            {
+                posts
+            };
+            ViewBag.List = list;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Models
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        //public DateTime Published { get; set; }
+    }
+}
